48f73fb25fd33a8b67387900187a8b6cd9e6e311
unknown <mabledevassyt@gmail.com>
2023-03-13 03:01:38 -0700

    ghfhvj

---
+++ script.js
@@ -0,0 +1,75 @@
+// var x= 20;
+// {
+//      var x=10;
+//      console.log(x);
+// }
+//  console.log(x);
+//  const pi=3.14;
+//  console.log(pi);
+//  var sum=5+10;
+//  console.log(sum);
+//  var p=5*10;
+//  console.log(p);
+//  var sub=10-5;
+//  console.log(sub);
+// var d=10/5;
+// console.log(d);
+// document.write("sum="+sum)
+// document.write("product="+p)
+// document.write(`sum is ${sum}`)
+// var student={
+//     name:"Mable",
+//     age:20,
+//     place:"Chalakudy"
+
+// }
+// console.log(student);
+// console.log(student.age);
+
+// var cars={
+//     model:"Hycross",
+//     colour:"Red",
+//     price:3500000
+// }
+// // console.log(cars.model);
+// var person=[
+//     {
+//         name:"Mable",
+//         age:20,
+//         place:"Chalakudy"
+//     },
+//     {
+//         name:"Krishna",
+//         age:21,
+//         place:"Mala"
+//     },
+//     {
+//         name:"siya",
+//         age:22,
+//         place:"Ekm"
+//     }
+// ]
+// console.log(person);
+// console.log(person[1].name);
+// const multiplication=(a,b)=>{
+//     var c=a*b;
+//     console.log(c)
+// }
+// multiplication(3,2)
+// 
+// 
+var fruits=["apple","orange","mango","grapes"]
+// for(var i=0;i<=3;i++)
+// {
+//     console.log(fruits[i]);
+// }
+// let p=0;
+// while(p<fruits.length){
+//     console.log(fruits[p]);
+//     p++;
+// } 
+let i=0;
+do{
+    console.log(fruits[i]);
+    i++
+}while(i<fruits.length)
